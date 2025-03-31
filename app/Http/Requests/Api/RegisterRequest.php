1034ff88fc384c9a9acd23822680c573c57f428f
safo4eg <stas.rakov111@mail.ru>
2025-03-31 16:47:11 +0700

    feat: добавлено обязательное поле password_confirmation

--- app/Http/Requests/Api/RegisterRequest.php
+++ app/Http/Requests/Api/RegisterRequest.php
@@ -28,7 +28,7 @@ class RegisterRequest extends FormRequest
             'client_secret' => ['required', 'string'],
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
         ];
     }
 
