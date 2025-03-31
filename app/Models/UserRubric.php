11c06b3bb5a9858fd7588287f2acacce94d72bbf
safo4eg <stas.rakov111@mail.ru>
2025-03-31 20:08:36 +0700

    fix: исправлено название связывающей таблицы для пользователя и рубрики

--- app/Models/UserRubric.php
+++ app/Models/UserRubric.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class Subscription extends Pivot
+class UserRubric extends Pivot
 {
     const UPDATED_AT = false;
     public $incrementing = false;
-    protected $table = 'subscriptions';
+    protected $table = 'user_rubric';
     protected $primaryKey = false;
     protected $guarded = [];
 
