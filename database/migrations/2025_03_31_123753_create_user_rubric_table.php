fc26f942a0e03936faeb6bb9634b580fcc39d1c3
safo4eg <stas.rakov111@mail.ru>
2025-04-02 01:11:29 +0700

    fix: исправил миграцию user_rubric

--- database/migrations/2025_03_31_123753_create_user_rubric_table.php
+++ database/migrations/2025_03_31_123753_create_user_rubric_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('user_rubric', function (Blueprint $table) {
-            $table->id();
             $table->foreignId('user_id')
                 ->constrained('users')
                 ->onDelete('cascade');
